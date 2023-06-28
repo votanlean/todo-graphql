e80222156c568c2a7f5ae0fe390a2b2da8d37097
Alex Vo <votanlean@gmail.com>
2023-06-28 17:05:49 +0700

    Support caching quote

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Features:
 - [x] Display dummy quote on Frontend
 - [x] Server datasource to get quotes from https://api-ninjas.com/api/quotes
-- [ ] Caching old quote for 1 minute to prevent abuse the API
+- [x] Caching old quote for 5 seconds to prevent abuse the API
 - [ ] Support dark mode
 - [ ] Validate empty task creation
--- server/src/datasources/quoteAPI.ts
+++ server/src/datasources/quoteAPI.ts
@@ -1,13 +1,32 @@
+import { DataSourceConfig } from 'apollo-datasource';
 import { RESTDataSource } from 'apollo-datasource-rest';
-
+import { InMemoryLRUCache } from 'apollo-server-caching'
 export class QuoteAPI extends RESTDataSource {
+  cache;
   constructor() {
     super();
     this.baseURL = 'https://api.quotable.io/';
   }
 
+  initialize(config: DataSourceConfig<any>): void {
+    super.initialize(config);
+    this.cache = config.cache || new InMemoryLRUCache();
+  }
+
+  cacheKey() {
+    // This API returns a random quote, so we can just use the same cache key
+    // Otherwise we would need to use id for the cache key
+    return `quote-quotableio`;
+  }
+
   async getRandomQuote() {
+    const cachedQuote = await this.cache.get(this.cacheKey());
+    if (cachedQuote) {
+      return cachedQuote;
+    }
     const quote = await this.get('random');
-    return {id: quote._id, ...quote};
+    const myQuote = {id: quote._id, ...quote};
+    await this.cache.set(this.cacheKey(), myQuote, {ttl: 5});// 5 seconds
+    return myQuote;
   }
 }
