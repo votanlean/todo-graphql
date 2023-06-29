960c5cf80661ff1d14dbf0534b44b731863397d7
Alex Vo <votanlean@gmail.com>
2023-06-29 11:28:50 +0700

    Client get schema from server code instead of API endpoint

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,9 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
-import dotenv from "dotenv";
-dotenv.config({path: '.env.local', override: true});
 const config: CodegenConfig = {
-  schema: process.env.VITE_SERVER_HOST || 'http://localhost:4000/graphql',
-  documents: "src/**/*.tsx",
+  schema: '../server/src/schema.ts',
+  documents: 'src/**/*.tsx',
   generates: {
     "./src/__generated__/": {
       preset: "client",
